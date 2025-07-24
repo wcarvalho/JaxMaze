fixed rotation

housemaze/env.py
@@ -151,12 +151,14 @@ class EnvState:
   grid: jax.Array
   agent_pos: jax.Array
   agent_dir: int
-  rotation: Tuple[int, int] = (0, 0)
 
   # task info
   map_idx: jax.Array
   task_w: jax.Array
   task_state: Optional[TaskState] = None
+
+  # more map info
+  rotation: Tuple[int, int] = (0, 0)
 
 
 
@@ -323,7 +325,7 @@ class HouseMaze:
     )
     if state.rotation is not None:
       start = num_object_categories + num_directions + H + W + self.num_actions()
-      rotation = start + state.rotation.astype(jnp.int32).sum()
+      rotation = start + jnp.asarray(state.rotation).astype(jnp.int32).sum()
       observation = observation.replace(
         rotation=rotation)
 

housemaze/utils.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 import numpy as np
 import pickle
 from housemaze.env import KeyboardActions
-
+from utils import MapInit
 
 def replace_color(image, old_color, new_color):
   # Convert the image and colors to JAX arrays if they aren't already
@@ -229,6 +229,7 @@ def from_str(
   char_to_key: dict,
   object_to_index: Optional[dict] = None,
   check_grid_letters: bool = True,
+  return_map_init: bool = True,
 ):
   level_str = level_str.strip()
   rows = level_str.split("\n")
@@ -284,6 +285,12 @@ def from_str(
   assert agent_pos is not None
   assert agent_dir is not None
 
+  if return_map_init:
+    return MapInit(
+      grid=jnp.asarray(grid),
+      agent_pos=jnp.asarray(agent_pos),
+      agent_dir=jnp.asarray(agent_dir)
+    )
   return grid, agent_pos, agent_dir
 
 